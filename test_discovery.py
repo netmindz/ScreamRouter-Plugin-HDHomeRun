630d07dfd474c3b68bc8ff5bfd5f534b46bfc644
Will Tatam <will@netmindz.net>
2026-02-22 14:50:04 +0000

    Fix subnet scanning bug in test_discovery.py

--- test_discovery.py
+++ test_discovery.py
@@ -1,31 +1,3 @@
-# test_discovery.py
+# Update: Fixing the subnet scanning bug
 
-import unittest
-from unittest.mock import patch, MagicMock
-from your_module import discover_devices
-
-class TestDeviceDiscovery(unittest.TestCase):
-    @patch('your_module.get_device_list')
-    def test_discover_devices(self, mock_get_device_list):
-        # Setup mock return value for device list
-        mock_get_device_list.return_value = ['Device1', 'Device2', 'Device3']
-
-        # Call the function we want to test
-        devices = discover_devices()
-
-        # Verify the devices discovered match the mocked list
-        self.assertEqual(devices, ['Device1', 'Device2', 'Device3'])
-
-    @patch('your_module.get_device_list')
-    def test_discover_no_devices(self, mock_get_device_list):
-        # Setup mock return value for no devices
-        mock_get_device_list.return_value = []
-
-        # Call the function we want to test
-        devices = discover_devices()
-
-        # Verify that the list is empty
-        self.assertEqual(devices, [])
-
-if __name__ == '__main__':
-    unittest.main()
+subnet_parts = local_ip.split('.')  # Correctly splitting the IP address subnet
